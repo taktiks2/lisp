6dc40d95143495c6ba74d4c1986c2e2edf9b7f6a
taktiks2 <deathproof.lee@gmail.com>
2024-09-25 23:31:32 +0900

    update

--- wumpus.lisp
+++ wumpus.lisp
@@ -8,33 +8,58 @@
 (defparameter *worm-num* 3)
 (defparameter *cop-odds* 15)
 
+;; 指定された範囲内でランダムな整数を生成する関数
+;; exp: (random-node) => 1
 (defun random-node ()
   (1+ (random *node-num*)))
 
+;; 2つのノードを結ぶエッジを生成する関数
+;; exp: (edge-pair 'a 'b) => ((a . b) (b . a))
 (defun edge-pair (a b)
-  (unless (eql a b)
-    (list (cons a b) (cons b a))))
+  (unless (eql a b) ; unless: 条件が満たされない場合にのみ実行される
+    (list (cons a b) (cons b a)))) ; list: 引数をリストにまとめる
 
+;;; funcallとapplyの違い
+;;; funcall exp: (funcall #'max 1 2 3) => 3
+;;; apply exp: (apply #'max '(1 2 3)) => 3
+;;; funcallは引数を個別に列挙する
+;;; applyは引数にリストを渡す
+
+;; ランダムな数字のエッジのリストを生成する関数
+;; exp: (make-edge-list) => ((2 . 3) (3 . 2) (5 . 1) (1 . 5) (4 . 7) (7 . 4))
 (defun make-edge-list ()
-  (apply #'append (loop repeat *edge-num*
-                        collect (edge-pair (random-node) (random-node)))))
+  (apply #'append (loop repeat *edge-num* ; append: 複数のリストを結合して一つのリストにする
+                        collect (edge-pair (random-node) (random-node))))) ; loop repeat {num} collect: {num}回繰り返し、collectでリストを生成する
 
-;; ここから
+;; nodeが始点であるエッジだけを抽出する関数
+;; exp: (direct-edges 'a '((a . b) (a . c) (b . a) (c . a)) => ((a . b) (a . c))
 (defun direct-edges (node edge-list)
-  (remove-if-not (lambda (x)
-                   (eql (car x) node))
+  (remove-if-not (lambda (x) ; remove-if-not: 指定された条件に合わない要素をリストから除外する
+                   (eql (car x) node)) ; eql: ２つのオブジェクトが同一であるか検証する
                  edge-list))
 
+;;; mapcとmapの違い
+;;; mapc exp: (mapc #'print '(1 2 3)) =>
+;;; 1
+;;; 2
+;;; 3
+;;; (1 2 3)
+;;; map exp: (map 'list' #'1+ '(1 2 3)) => (2 3 4)
+;;; mapcは関数の戻り値を無視して、引数のリストを返す
+;;; mapは関数の戻り値をリストにして返す
+
+;; あるnodeに接続しているnodeをすべて取得する関数
+;; exp: (get-connected 'a '((a . b) (a . c) (b . a) (c . a))) => (a b c)
 (defun get-connected (node edge-list)
-  (let ((visited nil))
-    (labels ((traverse (node)
-               (unless (member node visited)
-                 (push node visited)
-                 (mapc (lambda (edge)
+  (let ((visited nil)) ; visited: 訪れたノードを記録する変数を宣言
+    (labels ((traverse (node) ; traverse: 深さ優先探索を行う再帰関数を宣言
+               (unless (member node visited) ; member: リストに指定された要素が含まれているか検証する
+                 (push node visited) ; push: リストの先頭に要素を追加する
+                 (mapc (lambda (edge) ; mapc: リストの各要素に対して関数を適用する
                          (traverse (cdr edge)))
-                       (direct-edges node edge-list)))))
-      (traverse node))
-    visited))
+                       (direct-edges node edge-list))))) ; 各エッジペアに対してtraverseを適用する
+      (traverse node)) ; 宣言したtraverseを実行
+    visited)) ; 訪れたノードを返す
 
 (defun find-islands (nodes edge-list)
   (let ((islands nil))
